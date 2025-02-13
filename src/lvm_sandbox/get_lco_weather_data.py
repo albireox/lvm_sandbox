c9c6fe8777ec1ac7c69f5e37374252db88d6d8e2
José Sánchez-Gallego <gallegoj@uw.edu>
2025-02-13 00:10:55 +0000

    Add rain sensor data

--- src/lvm_sandbox/get_lco_weather_data.py
+++ src/lvm_sandbox/get_lco_weather_data.py
@@ -9,11 +9,67 @@
 from __future__ import annotations
 
 import datetime
+import json
+import os
 
 import polars
+from influxdb_client.client.influxdb_client import InfluxDBClient
 from lvmopstools.weather import get_weather_data
 from rich import print
 from rich.progress import track
+
+
+def get_rain_sensor(start_date: str = "2024-06-25"):
+    """Gets the rain sensor data."""
+
+    query = f"""
+    from(bucket: "actors")
+        |> range(start: {start_date}, stop: now())
+        |> filter(fn: (r) => r["_measurement"] == "lvmecp")
+        |> filter(fn: (r) => r["_field"] == "registers.rain_sensor_alarm")
+        |> toInt()
+        |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)
+        |> yield(name: "mean")
+    """
+
+    with InfluxDBClient(
+        url="http://10.8.38.26:9999",
+        token=os.environ["INFLUXDB_V2_TOKEN"],
+        org="LVM",
+        timeout=600000,
+    ) as client:
+        api = client.query_api()
+        query_results = api.query(query)
+
+    df = polars.DataFrame(
+        json.loads(query_results.to_json()),
+        schema={"_time": polars.String, "_value": polars.Float32},
+    ).select(
+        ts=polars.col._time.cast(polars.Datetime("ms", time_zone="UTC")),
+        rain=polars.col._value > 0,
+    )
+
+    return df
+
+
+def add_rain_sensor_data(data: polars.DataFrame):
+    """Adds rain sensor data to the weather data."""
+
+    print("[gray]Getting rain sensor data.[/gray]")
+
+    data = data.select(polars.all().exclude("rain_intensity"))
+    rain_data = get_rain_sensor(
+        start_date=data["ts"].min().date().strftime("%Y-%m-%d")  # type: ignore
+    ).sort("ts")
+
+    data = data.sort("ts").join_asof(
+        rain_data,
+        on="ts",
+        strategy="nearest",
+        tolerance="5m",
+    )
+
+    return data.sort("ts")
 
 
 async def get_lco_weather_data(backdays: int = 360):
@@ -24,7 +80,7 @@ async def get_lco_weather_data(backdays: int = 360):
 
     dfs: list[polars.DataFrame] = []
 
-    for _ in track(range(backdays)):
+    for _ in track(range(backdays), transient=True):
         start_date -= datetime.timedelta(days=1)
 
         # If we ask for 24 hours of data the API only returns a few hours so
@@ -38,6 +94,6 @@ async def get_lco_weather_data(backdays: int = 360):
                 dfs.append(data)
             except Exception:
                 print(f"[yellow]Error or end of data reached at {start_date}.[/yellow]")
-                return polars.concat(dfs)
+                return add_rain_sensor_data(polars.concat(dfs))
 
-    return polars.concat(dfs)
+    return add_rain_sensor_data(polars.concat(dfs))
