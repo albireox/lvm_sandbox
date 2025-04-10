b8a824a77aad0b0250aa6d09a74c9ebd3673b713
José Sánchez-Gallego <gallegoj@uw.edu>
2025-04-09 18:41:45 -0600

    Update focus_temperature.py and add collect_coadd_pointings.py

---
+++ src/lvm_sandbox/collect_coadd_pointings.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2025-04-08
+# @Filename: collect_coadd_pointings.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+import os
+import pathlib
+import re
+from functools import lru_cache
+
+import numpy
+import polars
+from astropy.io import fits
+from astropy.wcs import WCS
+from rich.progress import track
+
+
+AGCAM_ROOT = "/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/data/agcam/lco"
+SPECTRO_ROOT = "/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/lvm/spectro/data/lco/"
+
+DITHER_OFFSETS = {
+    0: [0.00, 0.00],
+    1: [-10.68, 18.50],
+    2: [10.68, 18.50],
+    3: [0.00, -12.33],
+    4: [10.68, -6.17],
+    5: [-10.68, -6.17],
+    6: [10.68, 6.17],
+    7: [-10.68, 6.17],
+    8: [0.00, 12.33],
+}
+
+
+def get_rot_matrix(angle: float):
+    """Returns a rotation matrix for the given angle in degrees."""
+
+    angle = angle * 3.141592653589793 / 180.0
+    cos_angle = numpy.cos(angle)
+    sin_angle = numpy.sin(angle)
+
+    return numpy.array([[cos_angle, -sin_angle], [sin_angle, cos_angle]])
+
+
+@lru_cache()
+def get_coadd_images():
+    """Gets the co-add images from the AGCAM data directory."""
+
+    # Get the list of co-add images
+    coadd_images: list[pathlib.Path] = []
+
+    # Loop through the AGCAM data directory and get the co-add images
+    dirs = os.listdir(AGCAM_ROOT)
+    for dir in sorted(dirs):
+        if not dir.startswith("6"):
+            continue
+
+        coadds_path = pathlib.Path(AGCAM_ROOT) / dir / "coadds"
+        if not coadds_path.exists():
+            continue
+
+        sci_files = sorted(coadds_path.glob("lvm.sci.coadd_s*.fits"))
+        for sci_file in sci_files:
+            coadd_images.append(sci_file.absolute())
+
+    return coadd_images
+
+
+def collect_coadd_pointings():
+    """Collects pointing data from the co-add images for the sci telescope."""
+
+    print("Collecting co-add images ...")
+    coadd_images = get_coadd_images()
+
+    output_file = "coadd_pointings.parquet"
+    if os.path.exists(output_file):
+        df = polars.read_parquet(output_file)
+    else:
+        df = None
+
+    new_data: list[dict] = []
+    for coadd_image in track(coadd_images):
+        mjd = int(coadd_image.parts[-3])
+
+        exposure_match = re.match(r"^lvm\.sci\.coadd_s(\d+)\.fits$", coadd_image.name)
+        if exposure_match is None:
+            continue
+
+        exposure_no = int(exposure_match.group(1))
+        if df is not None and (df["exposure_no"] == exposure_no).any():
+            continue
+
+        spec_image_path = (
+            pathlib.Path(SPECTRO_ROOT)
+            / str(mjd)
+            / f"sdR-s-b1-{exposure_no:08d}.fits.gz"
+        )
+
+        if not spec_image_path.exists():
+            continue
+
+        header = fits.getheader(coadd_image, 1)
+
+        if not header["SOLVED"]:
+            continue
+
+        spectro_header = fits.getheader(spec_image_path, 0)
+
+        tile_id = spectro_header["TILE_ID"]
+        if tile_id is None or tile_id <= 0:
+            continue
+
+        if "SMJD" not in spectro_header:
+            continue
+
+        try:
+            dpos = int(spectro_header["DPOS"])
+        except ValueError:
+            continue
+
+        wcs = WCS(header)
+        wcs_pointing = wcs.pixel_to_world(2500, 1000)
+        wcs_ra = float(wcs_pointing.ra.deg)
+        wcs_dec = float(wcs_pointing.dec.deg)
+
+        field_ra = header["RAFIELD"]
+        field_dec = header["DECFIELD"]
+        field_pa = header["PAFIELD"]
+
+        dpos_offset = DITHER_OFFSETS[dpos]
+        rot_matrix = get_rot_matrix(-(field_pa or 0.0))
+
+        dpos_offset_pa = numpy.dot(rot_matrix, dpos_offset) / 3600
+        ra_offset = field_ra - dpos_offset_pa[0] / numpy.cos(numpy.radians(field_dec))
+        dec_offset = field_dec - dpos_offset_pa[1]
+
+        new_data.append(
+            {
+                "mjd": int(spectro_header["SMJD"]),
+                "exposure_no": exposure_no,
+                "tile_id": tile_id,
+                "dpos": dpos,
+                "dpos_ra": dpos_offset[0],
+                "dpos_dec": dpos_offset[1],
+                "dpos_rot_ra": dpos_offset_pa[0],
+                "dpos_rot_dec": dpos_offset_pa[1],
+                "field_ra": field_ra,
+                "field_dec": field_dec,
+                "field_pa": field_pa,
+                "poscira": spectro_header.get("POSCIRA", None),
+                "poscidec": spectro_header.get("POSCIDE", None),
+                "poscipa": spectro_header.get("POSCIPA", None),
+                "wcs_ra": wcs_ra,
+                "wcs_dec": wcs_dec,
+                "ra_offset": ra_offset,
+                "dec_offset": dec_offset,
+            }
+        )
+
+    new_data_df = polars.from_records(
+        new_data,
+        schema={
+            "mjd": polars.Int32,
+            "exposure_no": polars.Int32,
+            "tile_id": polars.Int32,
+            "dpos": polars.Int32,
+            "dpos_ra": polars.Float64,
+            "dpos_dec": polars.Float64,
+            "dpos_rot_ra": polars.Float64,
+            "dpos_rot_dec": polars.Float64,
+            "field_ra": polars.Float64,
+            "field_dec": polars.Float64,
+            "field_pa": polars.Float64,
+            "poscira": polars.Float64,
+            "poscidec": polars.Float64,
+            "poscipa": polars.Float64,
+            "wcs_ra": polars.Float64,
+            "wcs_dec": polars.Float64,
+            "ra_offset": polars.Float64,
+            "dec_offset": polars.Float64,
+        },
+    )
+
+    if df is not None:
+        df = polars.concat([df, new_data_df])
+    else:
+        df = new_data_df
+
+    df = df.sort("exposure_no")
+    df.write_parquet(output_file)
+
+    return df
--- src/lvm_sandbox/focus_temperature.py
+++ src/lvm_sandbox/focus_temperature.py
@@ -2,146 +2,123 @@
 # -*- coding: utf-8 -*-
 #
 # @Author: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Date: 2023-08-14
+# @Date: 2025-04-08
 # @Filename: focus_temperature.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
 from __future__ import annotations
 
+import multiprocessing
 import pathlib
 
-import matplotlib
-import pandas
-import scipy.stats
-import seaborn
-from influxdb_client import InfluxDBClient
-from matplotlib import pyplot as plt
+import polars
+from astropy.io import fits
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    TextColumn,
+    TimeRemainingColumn,
+)
 
 
-matplotlib.use("Agg")
-seaborn.set_theme()
+AGCAM_ROOT = "/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/data/agcam/lco"
 
 
-TOKEN = "KNuEgMtZSsR-Bavk9ERgPgr7SDjQtcXLRqVEW-HG9q3Ixm1o7XXZTMywIybnmdJCdW61whpQ15jY0whljXRvfw=="  # noqa: E501
+def _process_mjd(dir: pathlib.Path):
+    """Processes an AGCAM MJD directory and collects focus sweep data."""
 
-OUTPATH = pathlib.Path(__file__).parents[2] / "outputs"
+    data: list[dict] = []
+
+    mjd = dir.parts[-1]
+
+    for telescope in ["sci", "spec", "skye", "skyw"]:
+        n_sweep = 0
+        was_sweep = False
+
+        files = sorted(dir.glob(f"lvm.{telescope}.agcam*.fits"))
+
+        for file in files:
+            image_no = int(file.stem.split("_")[-1])
+
+            guider_path = dir / f"lvm.{telescope}.guider_{image_no:08d}.fits"
+            if guider_path.exists():
+                continue
+
+            with fits.open(file) as hdul:
+                if "RAW" not in hdul or "PROC" not in hdul:
+                    continue
+
+                raw_header = hdul["RAW"].header
+                proc_header = hdul["PROC"].header
+
+                if "ISFSWEEP" not in proc_header or not proc_header["ISFSWEEP"]:
+                    was_sweep = False
+                    continue
+
+                if was_sweep is False:
+                    n_sweep += 1
+                    was_sweep = True
+
+                data.append(
+                    {
+                        "mjd": int(mjd),
+                        "image_no": image_no,
+                        "telescope": raw_header["TELESCOP"],
+                        "camname": raw_header["CAMNAME"],
+                        "bentempi": raw_header["BENTEMPI"],
+                        "bentempo": raw_header["BENTEMPO"],
+                        "benhumi": raw_header["BENHUMI"],
+                        "benhumo": raw_header["BENHUMO"],
+                        "focusdt": raw_header["FOCUSDT"],
+                        "airmass": raw_header["AIRMASS"],
+                        "fwhm": proc_header["FWHM"],
+                        "n_sweep": n_sweep,
+                    }
+                )
+
+    return data
 
 
-def get_telescope_data(telescope: str, outpath: pathlib.Path):
-    """Queries telescope data."""
+def collect_agcam_data():
+    """Collects focus sweep data from AGCAM files."""
 
-    client = InfluxDBClient("http://localhost:39999", org="LVM", token=TOKEN)
-    api = client.query_api()
+    agcam_path = pathlib.Path(AGCAM_ROOT)
+    dirs = sorted(agcam_path.glob("6*"))
 
-    query_focus = f"""
-        from(bucket: "actors")
-          |> range(start: -1mo)
-          |> filter(fn: (r) => r["_measurement"] == "lvm.{telescope}.guider")
-          |> filter(fn: (r) => r["_field"] == "frame.focus_position" or r["_field"] == "frame.fwhm")
-          |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-    """  # noqa: E501
+    data: list[dict] = []
 
-    data_focus = api.query_data_frame(query_focus)
-    data_focus = data_focus.drop(
-        columns=[
-            "table",
-            "result",
-            "_start",
-            "_stop",
-            "source",
-            "_measurement",
-        ]
+    with Progress(
+        TextColumn("[yellow]{task.description}"),
+        BarColumn(bar_width=None),
+        MofNCompleteColumn(),
+        TimeRemainingColumn(),
+        expand=True,
+        refresh_per_second=1,
+    ) as progress:
+        tid = progress.add_task("Processing AGCAM data ...", total=len(dirs))
+        with multiprocessing.Pool(processes=6) as pool:
+            data_imap = pool.imap(_process_mjd, dirs)
+            for dd in data_imap:
+                progress.update(tid, advance=1)
+                data += dd
+
+    df = polars.DataFrame(
+        data,
+        schema={
+            "mjd": polars.Int32,
+            "image_no": polars.Int32,
+            "telescope": polars.String,
+            "camname": polars.String,
+            "bentempi": polars.Float32,
+            "bentempo": polars.Float32,
+            "benhumi": polars.Float32,
+            "benhumo": polars.Float32,
+            "focusdt": polars.Float32,
+            "airmass": polars.Float32,
+            "fwhm": polars.Float32,
+            "n_sweep": polars.Int32,
+        },
     )
-    data_focus.dropna(inplace=True)
 
-    query_telemetry = f"""
-        from(bucket: "actors")
-          |> range(start: -1mo)
-          |> filter(fn: (r) => r["_measurement"] == "lvm.{telescope}.telemetry")
-          |> filter(fn: (r) => r["_field"] == "sensor1.temperature" or r["_field"] == "sensor2.temperature")
-          |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-
-    """  # noqa: E501
-
-    data_telemetry = api.query_data_frame(query_telemetry)
-    data_telemetry = data_telemetry.drop(
-        columns=[
-            "table",
-            "result",
-            "_start",
-            "_stop",
-            "source",
-            "_measurement",
-        ]
-    )
-    data_telemetry.dropna(inplace=True)
-
-    combined = pandas.merge_asof(data_focus, data_telemetry, "_time")
-    combined.rename(columns={"_time": "time"}, inplace=True)
-    combined.dropna(inplace=True)
-
-    combined = combined.loc[combined["frame.fwhm"] < 3]
-
-    combined.to_parquet(str(outpath))
-
-    return combined
-
-
-def fit_data(telescope: str, data: pandas.DataFrame):
-    """Fits focus-temperature."""
-
-    avg = data.groupby("frame.focus_position", as_index=False).mean()
-
-    fig, axes = plt.subplots(1, 2, figsize=(18, 10))
-
-    for sensor in [1, 2]:
-        if f"sensor{sensor}.temperature" in data:
-            slope, intercept, r, p, sterr = scipy.stats.linregress(
-                x=data[f"sensor{sensor}.temperature"],
-                y=data["frame.focus_position"],
-            )
-
-            print(f"{telescope}, sensor{sensor}")
-            print(
-                f"slope={slope:.3f}, intercept={intercept:.3f}, "
-                f"r={r:.3f}, p={p}, sterr={sterr:.6f}"
-            )
-
-        if f"sensor{sensor}.temperature" in data:
-            seaborn.scatterplot(
-                data=data,
-                x=f"sensor{sensor}.temperature",
-                y="frame.focus_position",
-                ax=axes[0],
-            )
-
-            seaborn.regplot(
-                data=avg,
-                x=f"sensor{sensor}.temperature",
-                y="frame.focus_position",
-                ax=axes[1],
-            )
-
-    fig.savefig(str(OUTPATH / f"focus_temperature_{telescope}.pdf"))
-
-    print()
-
-
-def focus_temperature():
-    """Determines the relationship focus-temperature."""
-
-    OUTPATH.mkdir(parents=True, exist_ok=True)
-
-    for telescope in ["sci", "spec", "skyw", "skye"]:
-        data_path = OUTPATH / f"focus_temperature_{telescope}.parquet"
-        if data_path.exists():
-            data = pandas.read_parquet(str(data_path))
-        else:
-            data = get_telescope_data(telescope, data_path)
-
-        assert isinstance(data, pandas.DataFrame)
-        fit_data(telescope, data)
-
-
-if __name__ == "__main__":
-    focus_temperature()
+    return df.sort(["mjd", "telescope", "image_no"])
